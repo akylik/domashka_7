ec8ead87c0a699861be587903e3dd83e27f23f73
Alexander Kulik <a_kylik@ukr.net>
2020-09-09 19:21:34 +0300

    Slider js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+   $('.slider').slick({
+      dots: true,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+      speed: 1000,
+      easing: 'ease',
+      infinite:false
+   });
+});
